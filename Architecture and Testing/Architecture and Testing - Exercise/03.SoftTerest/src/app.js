2ef5b7f8c4312915bd74e091ac834269fb621ff8
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-01-28 23:08:38 +0200

    Added SoftTerest task from Architecture and Testing Exercise

---
+++ Architecture and Testing/Architecture and Testing - Exercise/03.SoftTerest/src/app.js
@@ -0,0 +1,31 @@
+import { initialize } from "./router.js";
+import { logout } from "./api/users.js";
+import { showCatalog } from "./views/catalog.js";
+import { showCreate } from "./views/create.js";
+import { showDetails } from "./views/details.js";
+import { showHome } from "./views/home.js";
+import { showLogin } from "./views/login.js";
+import { showRegister } from "./views/register.js";
+
+document.getElementById('views').remove();
+
+const links = {
+    '/': showHome,
+    '/catalog': showCatalog,
+    '/login': showLogin,
+    '/register': showRegister,
+    '/details': showDetails,
+    '/create': showCreate,
+    '/logout': onLogout
+};
+
+const router = initialize(links);
+router.updateNav();
+
+router.goTo('/');
+
+function onLogout() {
+    logout();
+    router.updateNav();
+    router.goTo('/');
+}
