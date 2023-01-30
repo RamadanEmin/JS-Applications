293d90841d02c6cc80d8e14afdfc8dac0b38cc1e
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-01-30 15:06:35 +0200

    Added edit and delete recipe functionalities

--- Client Side Rendering/Client Side Rendering - Lab/02.Cookbook-Part-5/src/views/details.js
+++ Client Side Rendering/Client Side Rendering - Lab/02.Cookbook-Part-5/src/views/details.js
@@ -1,11 +1,13 @@
 import { html } from '../dom.js';
+import { getRecipeById, deleteRecipeById } from '../api/data.js';
 
-const detailsTemplate = (recipe, isOwner, nav) => html`
+const detailsTemplate = (recipe, isOwner, nav, onDelete) => html`
 <section id="details">
-    ${recipeCard(recipe, isOwner, nav.goTo)}
+    ${recipeCard(recipe, isOwner, nav.goTo, onDelete)}
+    ${showComments(recipe, nav)}
 </section>`;
 
-const recipeCard = (recipe, isOwner, goTo) => html`
+const recipeCard = (recipe, isOwner, goTo, onDelete) => html`
 <article>
     <h2>${recipe.name}</h2>
     <div class="band">
@@ -25,7 +27,7 @@ const recipeCard = (recipe, isOwner, goTo) => html`
         ? html`
     <div class="controls">
         <button @click=${() => goTo('edit', recipe._id)}>\u270E Edit</button>
-        <button>\u2716 Delete</button>
+        <button @click=${onDelete}>\u2716 Delete</button>
     </div>`
         : ''}
 </article>`;
@@ -40,6 +42,18 @@ export function setupDetails(nav) {
         const userId = sessionStorage.getItem('userId');
         const isOwner = userId != null && recipe._ownerId == userId;
 
-        return detailsTemplate(recipe, isOwner, nav);
+        return detailsTemplate(recipe, isOwner, nav, () => onDelete(recipe));
+    }
+
+    async function onDelete(recipe) {
+        const confirmed = confirm(`Are you sure you want to delete ${recipe.name}?`);
+        if (confirmed) {
+            try {
+                await deleteRecipeById(recipe._id);
+                nav.goTo('deleted');
+            } catch (err) {
+                alert(err.message);
+            }
+        }
     }
 }
