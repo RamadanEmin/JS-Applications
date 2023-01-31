5c8ca02276c9400757a7b29ad1a84f132ea9ff51
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-01-31 17:01:48 +0200

    Added Table-Search task from Client Side Rendering Exercise

---
+++ Client Side Rendering/Client Side Rendering - Exercise/05.Table-Search/solution.js
@@ -0,0 +1,37 @@
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+
+const studentRow = (student) => html`
+<tr class="${student.match ? 'select' : ''}">
+   <td>${student.item.firstName} ${student.item.lastName}</td>
+   <td>${student.item.email}</td>
+   <td>${student.item.course}</td>
+</tr>`;
+
+const input = document.getElementById('searchField');
+document.getElementById('searchBtn').addEventListener('click', onSearch);
+let students;
+
+start();
+
+async function start() {
+   const response = await fetch('http://localhost:3030/jsonstore/advanced/table');
+   const data = await response.json()
+   students = Object.values(data).map(s => ({ item: s, match: false }));
+
+   update();
+}
+
+function update() {
+   render(students.map(studentRow), document.querySelector('tbody'));
+}
+
+function onSearch() {
+   const value = input.value.trim().toLocaleLowerCase();
+
+   for (const student of students) {
+      student.match = Object.values(student.item).some(v => value && v.toLocaleLowerCase().includes(value))
+   }
+
+   input.value = '';
+   update();
+}
