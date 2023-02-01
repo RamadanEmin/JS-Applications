a63b4b80e7bf72cd4532de111df6b94f04159595
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-02-01 11:30:30 +0200

    Added Book-Library task from Client Side Rendering Exercise

---
+++ Client Side Rendering/Client Side Rendering - Exercise/06.Book-Library/src/create.js
@@ -0,0 +1,38 @@
+import { createBook, html } from "./utility.js";
+
+const createTemplate = (onSuccess) => html`
+<form @submit=${event=> onSubmit(event, onSuccess)} id="add-form">
+    <h3>Add book</h3>
+    <label>TITLE</label>
+    <input type="text" name="title" placeholder="Title...">
+    <label>AUTHOR</label>
+    <input type="text" name="author" placeholder="Author...">
+    <input type="submit" value="Submit">
+</form>`;
+
+export function showCreate(ctx) {
+    if (ctx.book === undefined) {
+        return createTemplate(ctx.update);
+    } else {
+        return null;
+    }
+
+}
+
+async function onSubmit(event, onSuccess) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    const title = formData.get('title').trim();
+    const author = formData.get('author').trim();
+
+    if (!title || !author) {
+        alert('All fields are required!');
+        return;
+    }
+
+    await createBook({ title, author });
+
+    event.target.reset();
+    onSuccess();
+}
