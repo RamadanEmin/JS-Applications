3de28f2c3ddff45193adf7eeebdc1a00b0995c21
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-02-02 21:01:17 +0200

    Added login, register and logout functionalities

--- Routing/Routing - Exercise/01.Furniture/src/api/data.js
+++ Routing/Routing - Exercise/01.Furniture/src/api/data.js
@@ -1,5 +1,10 @@
 import * as api from './api.js';
 
+export const login = api.login;
+export const register = api.register;
+export const logout = api.logout;
+
+
 const endpoints = {
   all: '/data/catalog',
   my: (id) => `/data/catalog?where=_ownerId%3D%22${id}%22`
--- Routing/Routing - Exercise/01.Furniture/src/app.js
+++ Routing/Routing - Exercise/01.Furniture/src/app.js
@@ -1,17 +1,43 @@
+import { logout } from './api/api.js';
 import { page, render } from './lib.js';
 import { getUserData, removeUserData } from './util.js';
 
 import { catalogView } from './views/catalog.js';
+import { loginView } from './views/login.js';
+import { registerView } from './views/register.js';
+
 
 const root = document.querySelector('div.container');
+document.getElementById('logoutBtn').addEventListener('click', onLogout);
 
 page(decorateContext);
 page('/', catalogView);
+page('/login', loginView);
+page('/register', registerView);
+page('/my-furniture', catalogView);
 
+updateUserNav();
 page.start();
 
 function decorateContext(ctx, next) {
   ctx.render = (content) => render(content, root);
   ctx.updateUserNav = updateUserNav;
   next();
+}
+
+async function updateUserNav() {
+  if (getUserData() !== null) {
+    document.getElementById('user').style.display = 'inline-block';
+    document.getElementById('guest').style.display = 'none';
+  } else {
+    document.getElementById('user').style.display = 'none';
+    document.getElementById('guest').style.display = 'inline-block';
+  }
+}
+
+async function onLogout() {
+  await logout();
+  removeUserData();
+  updateUserNav();
+  page.redirect('/');
 }
