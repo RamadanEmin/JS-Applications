b88406a8b06552843cd376ebf1c7c7d522b29f43
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-02-03 16:04:28 +0200

    Initial architecture, added Home page and  login, register, logout functionalities

---
+++ Modular Applications/Modular Applications - Lab/01.Cookbook-Part-7/src/app.js
@@ -0,0 +1,46 @@
+import page from '//unpkg.com/page/page.mjs';
+
+import { createNav } from './navigation.js';
+import { logout as apiLogout } from './api/data.js';
+
+import { setupHome } from './views/home.js';
+import { setupLogin, onLoginSubmit } from './views/login.js';
+import { setupRegister, onRegisterSubmit } from './views/register.js';
+
+window.addEventListener('load', async () => {
+    const main = document.querySelector('main');
+    const navbar = document.querySelector('nav');
+    const navigation = createNav(main, navbar);
+
+    const views = {
+        homeView: navigation.registerView('home', setupHome),
+        loginView: navigation.registerView('login', setupLogin, 'loginLink'),
+        registerView: navigation.registerView('register', setupRegister, 'registerLink'),
+    };
+
+    page('/', views.homeView);
+    page('/index.html', views.homeView);
+    page('/catalog', views.catalogView);
+
+    page('/login', views.loginView);
+    navigation.registerForm('loginForm', onLoginSubmit, () => { page.redirect('/'); navigation.setUserNav(); });
+
+    page('/register', views.registerView);
+    navigation.registerForm('registerForm', onRegisterSubmit, () => { page.redirect('/'); navigation.setUserNav(); });
+
+    document.getElementById('logoutBtn').addEventListener('click', logout);
+
+    // Start application
+    page();
+
+
+    async function logout() {
+        try {
+            await apiLogout();
+            navigation.setUserNav();
+            page.redirect('/catalog');
+        } catch (err) {
+            alert(err.message);
+        }
+    }
+});
