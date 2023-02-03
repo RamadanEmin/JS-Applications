cbfdd648bcd6c8b49d1852962873957be32ddbec
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-02-03 21:41:20 +0200

    Added option to add, edit and delete comment

--- Modular Applications/Modular Applications - Lab/01.Cookbook-Part-7/src/views/details.js
+++ Modular Applications/Modular Applications - Lab/01.Cookbook-Part-7/src/views/details.js
@@ -1,10 +1,12 @@
 import { html } from '../dom.js';
 import { getRecipeById, deleteRecipeById } from '../api/data.js';
+import { showComments } from './comments.js';
 
 
 const detailsTemplate = (recipe, isOwner, onDelete) => html`
 <section id="details">
     ${recipeCard(recipe, isOwner, onDelete)}
+    ${showComments(recipe)}
 </section>`;
 
 const recipeCard = (recipe, isOwner, onDelete) => html`
