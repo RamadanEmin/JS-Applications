630c1e832cdaf83f1141e1248f2390a42258a7a5
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-02-04 16:09:30 +0200

    Added login, register and logout functionalities

--- Modular Applications/Modular Applications - Exercise/01.Team-Manager/src/api/data.js
+++ Modular Applications/Modular Applications - Exercise/01.Team-Manager/src/api/data.js
@@ -2,3 +2,7 @@ import * as api from './api.js';
 
 const host = 'http://localhost:3030';
 api.settings.host = host;
+
+export const login = api.login;
+export const register = api.register;
+export const logout = api.logout;
--- Modular Applications/Modular Applications - Exercise/01.Team-Manager/src/app.js
+++ Modular Applications/Modular Applications - Exercise/01.Team-Manager/src/app.js
@@ -1,13 +1,20 @@
 import page from '../node_modules/page/page.mjs';
 import { render } from '../node_modules/lit-html/lit-html.js';
 
+import { logout as apiLogout } from './api/data.js';
 import { homePage } from './views/home.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
 
 const main = document.querySelector('main');
 document.getElementById('logoutBtn').addEventListener('click', logout);
 
 page('/', decorateContext, homePage);
+page('/index.html', decorateContext, homePage);
+page('/login', decorateContext, loginPage);
+page('/register', decorateContext, registerPage);
 
+setUserNav();
 page.start();
 
 function decorateContext(ctx, next) {
@@ -15,4 +22,21 @@ function decorateContext(ctx, next) {
     ctx.render = (content) => render(content, main);
 
     next();
+}
+
+function setUserNav() {
+    const userId = sessionStorage.getItem('userId');
+    if (userId != null) {
+        [...document.querySelectorAll('nav > a.user')].forEach(a => a.style.display = 'block');
+        [...document.querySelectorAll('nav > a.guest')].forEach(a => a.style.display = 'none');
+    } else {
+        [...document.querySelectorAll('nav > a.user')].forEach(a => a.style.display = 'none');
+        [...document.querySelectorAll('nav > a.guest')].forEach(a => a.style.display = 'block');
+    }
+}
+
+async function logout() {
+    await apiLogout();
+    setUserNav();
+    page.redirect('/');
 }
