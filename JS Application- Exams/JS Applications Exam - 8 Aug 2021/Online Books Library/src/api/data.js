e7c47d5b64314882f1c63d49659f8725d1f8aaf1
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-02-09 18:14:02 +0200

    Added login, register and logout functionalities

--- JS Application- Exams/JS Applications Exam - 8 Aug 2021/Online Books Library/src/api/data.js
+++ JS Application- Exams/JS Applications Exam - 8 Aug 2021/Online Books Library/src/api/data.js
@@ -1,5 +1,9 @@
 import * as api from './api.js';
 
+export const login = api.login;
+export const register = api.register;
+export const logout = api.logout;
+
 export async function getAllBooks() {
     return api.get('/data/books?sortBy=_createdOn%20desc');
 }
--- JS Application- Exams/JS Applications Exam - 8 Aug 2021/Online Books Library/src/app.js
+++ JS Application- Exams/JS Applications Exam - 8 Aug 2021/Online Books Library/src/app.js
@@ -1,10 +1,15 @@
 import { page } from './lib.js';
 import { homePage } from './views/home.js';
-import decorateContext from './middlewares/decorateContext.js';
+import decorateContext, { updateUserNav } from './middlewares/decorateContext.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
 
 page(decorateContext);
 page('/', homePage);
+page('/login', loginPage);
+page('/register', registerPage);
 
+updateUserNav();
 page.start();
 
 
