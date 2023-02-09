ceb1b801a43d2c37d20f7f974a8d26ce538a1817
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-02-09 09:27:36 +0200

    Added login, register and logout functionalities

--- JS Application- Exams/JS Applications Exam - Meme Lounge/Meme_Lounge/src/app.js
+++ JS Application- Exams/JS Applications Exam - Meme Lounge/Meme_Lounge/src/app.js
@@ -1,12 +1,20 @@
 import { page, render } from './lib.js';
 
 import { homePage } from './views/home.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
+import { logout } from './api/data.js';
+import { getUserData } from './util.js';
 
 const root = document.querySelector('main');
+document.getElementById('logoutBtn').addEventListener('click', onLogout);
 
 page(decorateContext);
 page('/', homePage);
+page('/login', loginPage);
+page('/register', registerPage);
 
+updateUserNav();
 page.start();
 
 function decorateContext(ctx, next) {
@@ -14,4 +22,23 @@ function decorateContext(ctx, next) {
     ctx.updateUserNav=updateUserNav;
 
     next();
+}
+
+function onLogout() {
+    logout();
+    updateUserNav();
+    page.redirect('/');
+}
+
+function updateUserNav() {
+    const userData = getUserData();
+
+    if (userData) {
+        document.querySelector('.user').style.display = 'block';
+        document.querySelector('.guest').style.display = 'none';
+        document.querySelector('.user span').textContent = `Welcome, ${userData.email}`;
+    } else {
+        document.querySelector('.guest').style.display = 'block';
+        document.querySelector('.user').style.display = 'none';
+    }
 }
