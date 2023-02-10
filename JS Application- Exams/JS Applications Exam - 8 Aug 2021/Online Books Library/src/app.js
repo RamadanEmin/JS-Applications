381419d30f446048387dee16e223ff7857017f52
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-02-10 11:52:27 +0200

    Added details page
    All users can see book details.

--- JS Application- Exams/JS Applications Exam - 8 Aug 2021/Online Books Library/src/app.js
+++ JS Application- Exams/JS Applications Exam - 8 Aug 2021/Online Books Library/src/app.js
@@ -4,6 +4,7 @@ import decorateContext, { updateUserNav } from './middlewares/decorateContext.js
 import { loginPage } from './views/login.js';
 import { registerPage } from './views/register.js';
 import { createPage } from './views/create.js';
+import { detailsPage } from './views/details.js';
 import { myBooksPage } from './views/my-books.js';
 
 page(decorateContext);
@@ -11,6 +12,7 @@ page('/', homePage);
 page('/login', loginPage);
 page('/register', registerPage);
 page('/create', createPage);
+page('/details/:id', detailsPage);
 page('/my-books', myBooksPage);
 
 updateUserNav();
---
+++ JS Application- Exams/JS Applications Exam - 8 Aug 2021/Online Books Library/src/views/details.js
@@ -0,0 +1,44 @@
+import {getBookById} from '../api/data.js';
+import { html } from '../lib.js';
+import { getUserData } from '../util.js';
+
+const detailsTemplate = (book, isOwner) => html`
+<section id="details-page" class="details">
+    <div class="book-information">
+        <h3>${book.title}</h3>
+        <p class="type">Type: ${book.type}</p>
+        <p class="img"><img src=${book.imageUrl}></p>
+        <div class="actions">
+            ${bookControlsTemplate(book, isOwner, onDelete)}
+
+            <div class="likes">
+                <img class="hearts" src="/images/heart.png">
+                <span id="total-likes">Likes: </span>
+            </div>
+        </div>
+    </div>
+    <div class="book-description">
+        <h3>Description:</h3>
+        <p>${book.description}</p>
+    </div>
+</section>`;
+
+const bookControlsTemplate = (book, isOwner) => {
+    if (isOwner) {
+        return html`
+        <a class="button" href="/edit/${book._id}">Edit</a>
+        <a class="button" href="javascript:void(0)">Delete</a>`;
+    } else {
+        return null;
+    }
+}
+
+export async function detailsPage(ctx) {
+    const userData = getUserData();
+    const book =  getBookById(ctx.params.id);
+
+    const isOwner = userData && userData.id == book._ownerId;
+
+    ctx.render(detailsTemplate(book, isOwner));
+
+} 
