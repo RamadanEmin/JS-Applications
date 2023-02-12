127bf6dd41e06e25af55b7bc92f19918db2e4e8e
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-02-12 21:17:32 +0200

    Added login, register and logout functionalities

--- JS Application- Exams/JS Applications Exam - 4 December 2021/Music App/src/app.js
+++ JS Application- Exams/JS Applications Exam - 4 December 2021/Music App/src/app.js
@@ -3,11 +3,22 @@ import page from '../node_modules/page/page.mjs';
 import { addRender } from './middlewares/render.js';
 import { addSession } from './middlewares/session.js';
 
+import { logout } from './api/users.js';
 import { homePage } from './views/home.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
 
 page(addSession);
 page(addRender);
 
 page('/', homePage);
+page('/login', loginPage);
+page('/register', registerPage);
+page('/logout', onLogout);
 
 page.start();
+
+function onLogout(ctx) {
+    logout();
+    ctx.page.redirect('/');
+}
