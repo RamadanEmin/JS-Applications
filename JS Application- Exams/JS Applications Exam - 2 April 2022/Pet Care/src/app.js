ae47f780a5fbec5f014f4ef30a9594388b6c95d9
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-02-14 16:29:03 +0200

    Added login, register and logout functionalities

--- JS Application- Exams/JS Applications Exam - 2 April 2022/Pet Care/src/app.js
+++ JS Application- Exams/JS Applications Exam - 2 April 2022/Pet Care/src/app.js
@@ -4,11 +4,21 @@ import { addRender } from './middlewares/render.js';
 import { addSession } from './middlewares/session.js';
 
 import { homePage } from './views/home.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
+import { logout } from './api/users.js';
 
 page(addSession);
 page(addRender);
 
 page('/', homePage );
-
+page('/login', loginPage );
+page('/register', registerPage );
+page('/logout', onLogout );
 
 page.start();
+
+function onLogout(ctx) {
+    logout();
+    ctx.page.redirect('/');
+}
