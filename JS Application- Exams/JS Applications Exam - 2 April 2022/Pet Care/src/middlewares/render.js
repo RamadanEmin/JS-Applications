ed59814aa2a5653fd8570cebf37d490955683c26
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-02-14 13:10:49 +0200

    Initial architecture and added Home page

---
+++ JS Application- Exams/JS Applications Exam - 2 April 2022/Pet Care/src/middlewares/render.js
@@ -0,0 +1,30 @@
+import { html, render } from "../../node_modules/lit-html/lit-html.js";
+
+const navTemplate = (user) => html`
+<nav>
+    <section class="logo">
+        <img src="./images/logo.png" alt="logo">
+    </section>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/catalog">Dashboard</a></li>
+        ${user
+            ? html `<li><a href="create">Create Postcard</a></li>
+                    <li><a href="/logout">Logout</a></li>`
+            : html `<li><a href="/login">Login</a></li>
+                    <li><a href="/register">Register</a></li>`}
+    </ul>
+</nav>`;
+
+const header = document.querySelector('.my-header');
+const root = document.getElementById('content');
+
+function ctxRender(content) {
+    render(content, root);
+}
+
+export function addRender(ctx, next) {
+    render(navTemplate(ctx.user), header);
+    ctx.render = ctxRender;
+    next();
+}
