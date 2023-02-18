1c4b9f6bfb4cb7f319be2769474651e2eb51bc0f
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-02-18 21:21:53 +0200

    Added details page
    All view details about shoes.

--- JS Application- Exams/JS Applications Exam - 15 August 2022/Sole Mates/src/api/shop.js
+++ JS Application- Exams/JS Applications Exam - 15 August 2022/Sole Mates/src/api/shop.js
@@ -1,9 +1,14 @@
 import * as api from './api.js';
 
 const endpoints = {
+    shoeById: '/data/shoes/',
     allShoes: '/data/shoes?sortBy=_createdOn%20desc',
     add: '/data/shoes'
 };
+
+export async function getShoeById(shoeId) {
+    return api.get(endpoints.shoeById + shoeId);
+}
 
 export async function getAllShoes() {
     return api.get(endpoints.allShoes);
---
+++ JS Application- Exams/JS Applications Exam - 15 August 2022/Sole Mates/src/views/details.js
@@ -0,0 +1,40 @@
+import { html, nothing } from '../../node_modules/lit-html/lit-html.js';
+import * as shopServices from '../api/shop.js';
+
+const detailsTemplate = (shoe, isOwner) => html`
+<section id="details">
+    <div id="details-wrapper">
+        <p id="details-title">Shoe Details</p>
+        <div id="img-wrapper">
+            <img src=${shoe.imageUrl} alt="example1" />
+        </div>
+        <div id="info-wrapper">
+            <p>Brand: <span id="details-brand">${shoe.brand}</span></p>
+            <p>
+                Model: <span id="details-model">${shoe.model}</span>
+            </p>
+            <p>Release date: <span id="details-release">${shoe.release}</span></p>
+            <p>Designer: <span id="details-designer">${shoe.designer}</span></p>
+            <p>Value: <span id="details-value">${shoe.value}</span></p>
+        </div>
+
+        ${isOwner
+            ? html `<div id="action-buttons">
+                        <a href="/edit/${shoe._id}" id="edit-btn">Edit</a>
+                        <a href="javascript:void(0)" id="delete-btn">Delete</a>
+                    </div>`
+            : nothing
+        }
+        
+    </div>
+</section>`;
+
+
+export async function detailsPage(ctx) {
+    const shoeId = ctx.params.id;
+    const shoe = await shopServices.getShoeById(shoeId);
+
+    const isOwner = ctx.user && ctx.user._id === shoe._ownerId;
+
+    ctx.render(detailsTemplate(shoe, isOwner));
+}
