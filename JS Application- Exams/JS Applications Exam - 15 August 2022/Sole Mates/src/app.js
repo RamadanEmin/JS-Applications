6d7871de857689a7e6758d1d21b45f6f8993af96
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-02-18 10:48:58 +0200

    Added login, register and logout functionalities

--- JS Application- Exams/JS Applications Exam - 15 August 2022/Sole Mates/src/app.js
+++ JS Application- Exams/JS Applications Exam - 15 August 2022/Sole Mates/src/app.js
@@ -4,10 +4,16 @@ import { addRender } from './middlewares/render.js';
 import { addSession } from './middlewares/session.js';
 
 import { homePage } from './views/home.js';
+import { registerPage } from './views/register.js';
+import { loginPage } from './views/login.js';
+import { logout } from './api/users.js';
 
 page(addSession);
 page(addRender);
 
 page('/', homePage);
+page('/register', registerPage);
+page('/login', loginPage);
+page('/logout', onLogout);
 
 page.start();
