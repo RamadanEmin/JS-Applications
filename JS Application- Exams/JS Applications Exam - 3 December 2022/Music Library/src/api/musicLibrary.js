36c782e3c2e99073c48e82ddc7fbfffedc6c0d98
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-02-20 10:53:54 +0200

    Added create page
    The Create page is available to logged-in users. It contains a form for adding a new album.

--- JS Application- Exams/JS Applications Exam - 3 December 2022/Music Library/src/api/musicLibrary.js
+++ JS Application- Exams/JS Applications Exam - 3 December 2022/Music Library/src/api/musicLibrary.js
@@ -1,9 +1,14 @@
 import * as api from './api.js';
 
 const endpoints = {
-    allAlbums: '/data/albums?sortBy=_createdOn%20desc'
+    allAlbums: '/data/albums?sortBy=_createdOn%20desc',
+    addAlbum: '/data/albums'
 };
 
 export async function getAllAlbums() {
     return api.get(endpoints.allAlbums);
+}
+
+export async function addNewAlbum(data) {
+    return api.post(endpoints.addAlbum, data);
 }
---
+++ JS Application- Exams/JS Applications Exam - 3 December 2022/Music Library/src/views/create.js
@@ -0,0 +1,43 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import * as musicLibraryServices from '../api/musicLibrary.js';
+import { createSubmitHandler } from '../util.js';
+
+const createTemplate = (onSubmit) => html`
+<section id="create">
+    <div class="form">
+        <h2>Add Album</h2>
+        <form @submit=${onSubmit} class="create-form">
+            <input type="text" name="singer" id="album-singer" placeholder="Singer/Band" />
+            <input type="text" name="album" id="album-album" placeholder="Album" />
+            <input type="text" name="imageUrl" id="album-img" placeholder="Image url" />
+            <input type="text" name="release" id="album-release" placeholder="Release date" />
+            <input type="text" name="label" id="album-label" placeholder="Label" />
+            <input type="text" name="sales" id="album-sales" placeholder="Sales" />
+
+            <button type="submit">post</button>
+        </form>
+    </div>
+</section>`;
+
+export function createPage(ctx) {
+
+    ctx.render(createTemplate(createSubmitHandler(ctx, onSubmit)));
+}
+
+async function onSubmit(ctx, data, event) {
+    if (Object.values(data).some(v => v == '')) {
+        return alert('All fields are required!');
+    }
+
+    await musicLibraryServices.addNewAlbum({
+        singer: data.singer,
+        album: data.album,
+        imageUrl: data.imageUrl,
+        release: data.release,
+        label: data.label,
+        sales: data.sales
+    });
+
+    event.target.reset();
+    ctx.page.redirect('/catalog');
+}
