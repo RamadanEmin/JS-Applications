53ecf09c3af8531d0edf1c16932ee856566e77f6
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-02-21 17:06:51 +0200

    Added buy / delete functionality
    Every logged-in user able to buy products, but not his own. By clicking on the [Buy] button, the counter of each product bought increases by 1.

--- JS Application- Exams/JS Applications Exam - 13 Dec 2022/Cosmetic Kingdom/src/api/cosmetic.js
+++ JS Application- Exams/JS Applications Exam - 13 Dec 2022/Cosmetic Kingdom/src/api/cosmetic.js
@@ -4,6 +4,10 @@ const endpoints = {
     product: '/data/products/',
     allProducts: '/data/products?sortBy=_createdOn%20desc',
     add: '/data/products',
+    delete: '/data/products/',
+    buy: '/data/bought',
+    pressed: (productId, userId) => `/data/bought?where=productId%3D%22${productId}%22%20and%20_ownerId%3D%22${userId}%22&count`,
+    buys: (productId) => `/data/bought?where=productId%3D%22${productId}%22&distinct=_ownerId&count`
 };
 
 export async function getProduct(productId) {
@@ -16,4 +20,20 @@ export async function getAllProducts() {
 
 export async function addNewProduct(data) {
     return api.post(endpoints.add, data);
+}
+
+export async function deleteProduct(productId) {
+    return api.del(endpoints.delete + productId);
+}
+
+export async function addBuys(data) {
+    return api.post(endpoints.buy, data);
+}
+
+export async function presetBuy(productId, userId) {
+    return api.get(endpoints.pressed(productId, userId));
+}
+
+export async function getAllBuys(productId) {
+    return api.get(endpoints.buys(productId));
 }
--- JS Application- Exams/JS Applications Exam - 13 Dec 2022/Cosmetic Kingdom/src/views/details.js
+++ JS Application- Exams/JS Applications Exam - 13 Dec 2022/Cosmetic Kingdom/src/views/details.js
@@ -1,7 +1,7 @@
 import { html, nothing } from '../../node_modules/lit-html/lit-html.js';
 import * as cosmeticServices from '../api/cosmetic.js';
 
-const detailsTemplate = (product, user, isOwner) => html`
+const detailsTemplate = (product, user, isOwner, buys, hasBuy, onDelete, onBuy) => html`
 <section id="details">
     <div id="details-wrapper">
         <img id="details-img" src=${product.imageUrl} alt="example1" />
@@ -13,17 +13,17 @@ const detailsTemplate = (product, user, isOwner) => html`
             Price: <span id="price-number">${Number(product.price)}</span>$</p>
         <div id="info-wrapper">
             <div id="details-description">
-                <h4>Bought: <span id="buys">0</span> times.</h4>
+                <h4>Bought: <span id="buys">${buys}</span> times.</h4>
                 <span>${product.description}</span>
             </div>
         </div>
 
-        ${user
+        ${user && !hasBuy
             ? html `<div id="action-buttons">
                 ${isOwner
                     ? html `<a href="/edit/${product._id}" id="edit-btn">Edit</a>
-                            <a href="javascript:void(0)" id="delete-btn">Delete</a>`
-                    : html `<a href="javascript:void(0)" id="buy-btn">Buy</a>`}
+                            <a @click=${onDelete} href="javascript:void(0)" id="delete-btn">Delete</a>`
+                    : html `<a @click=${onBuy} href="javascript:void(0)" id="buy-btn">Buy</a>`}
                 </div>`
             : nothing
         }
@@ -33,9 +33,33 @@ const detailsTemplate = (product, user, isOwner) => html`
 
 export async function detailsPage(ctx) {
     const productId = ctx.params.id;
-    const product = await cosmeticServices.getProduct(productId);
+    const request = [
+        cosmeticServices.getProduct(productId),
+        cosmeticServices.getAllBuys(productId)
+    ];
+
+    if (ctx.user) {
+        request.push(cosmeticServices.presetBuy(productId, ctx.user._id));
+    }
+
+    const [product, buys, hasBuy] = await Promise.all(request);
 
     const isOwner = ctx.user && ctx.user._id === product._ownerId;
 
-    ctx.render(detailsTemplate(product, ctx.user, isOwner));
+    ctx.render(detailsTemplate(product, ctx.user, isOwner, buys, hasBuy, onDelete, onBuy));
+
+    async function onDelete() {
+        const choice = confirm('Are you sure you want to delete this product?');
+        if (choice) {
+            await cosmeticServices.deleteProduct(productId);
+
+            ctx.page.redirect('/catalog');
+        }
+    }
+
+    async function onBuy() {
+        await cosmeticServices.addBuys({ productId });
+
+        ctx.page.redirect('/details/' + productId)
+    }
 }
