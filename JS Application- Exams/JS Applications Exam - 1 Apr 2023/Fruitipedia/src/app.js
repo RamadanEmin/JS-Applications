eb87159330249249cfa75a5da115bcc89f357e1b
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-02 21:37:10 +0200

    Added Register, Login, Logout page and functionality

--- JS Application- Exams/JS Applications Exam - 1 Apr 2023/Fruitipedia/src/app.js
+++ JS Application- Exams/JS Applications Exam - 1 Apr 2023/Fruitipedia/src/app.js
@@ -4,27 +4,17 @@ import { addRender } from './middlewares/render.js';
 import { addSession } from './middlewares/session.js';
 
 import { homePage } from './views/home.js';
-import { catalogPage } from './views/catalog.js';
 import { registerPage } from './views/register.js';
 import { loginPage } from './views/login.js';
 import { logout } from './api/users.js';
-import { createPage } from './views/create.js';
-import { detailsPage } from './views/details.js';
-import { editPage } from './views/edit.js';
-import { searchPage } from './views/search.js';
 
 page(addSession);
 page(addRender);
 
 page('/', homePage);
-page('/catalog', catalogPage);
 page('/register', registerPage);
 page('/login', loginPage);
 page('/logout', onLogout);
-page('/create', createPage);
-page('/details/:id', detailsPage);
-page('/edit/:id', editPage);
-page('/search', searchPage);
 
 page.start();
 
