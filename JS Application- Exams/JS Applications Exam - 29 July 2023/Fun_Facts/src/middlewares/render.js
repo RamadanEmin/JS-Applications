52f0465aed142b364282903205802a2481d8eaca
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-03 10:39:56 +0200

    Initial project and added Home page

---
+++ JS Application- Exams/JS Applications Exam - 29 July 2023/Fun_Facts/src/middlewares/render.js
@@ -0,0 +1,36 @@
+import { html, render } from "../../node_modules/lit-html/lit-html.js";
+
+const navTemplate = (user) => html`
+ <a id="logo" href="/"
+          ><img id="logo-img" src="./images/logo.png" alt=""
+        /></a>
+
+        <nav>
+          <div>
+            <a href="/catalog">Fun Facts</a>
+          </div>
+    ${user
+         ? html `<div class="user">
+            <a href="/create">Add Fact</a>
+            <a href="/logout">Logout</a>
+          </div>`
+          : html `<div class="guest">
+            <a href="/login">Login</a>
+            <a href="/register">Register</a>
+            
+          </div>`}
+        </nav>
+`;
+
+const header = document.querySelector('.my-header');
+const root = document.getElementById('main-content');
+
+function ctxRender(content) {
+    render(content, root);
+}
+
+export function addRender(ctx, next) {
+     render(navTemplate(ctx.user), header);
+    ctx.render = ctxRender;
+    next();
+}
