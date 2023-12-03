b20bd34fdbe6d84394b4a3b778363aa02104d55b
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-03 10:03:02 +0200

    Added Details page

--- JS Application- Exams/JS Applications Exam - 1 Apr 2023/Fruitipedia/src/api/fruits.js
+++ JS Application- Exams/JS Applications Exam - 1 Apr 2023/Fruitipedia/src/api/fruits.js
@@ -1,9 +1,14 @@
 import * as api from './api.js';
 
 const endpoints = {
+    getOne: '/data/fruits/',
     getAll: '/data/fruits?sortBy=_createdOn%20desc',
-    create: '/data/fruits'
+    create: '/data/fruits',
 };
+
+export async function getFruit(fruitId) {
+    return api.get(endpoints.getOne + fruitId);
+}
 
 export async function getAllFruits() {
     return api.get(endpoints.getAll);
---
+++ JS Application- Exams/JS Applications Exam - 1 Apr 2023/Fruitipedia/src/views/details.js
@@ -0,0 +1,38 @@
+import { html, nothing } from "../../node_modules/lit-html/lit-html.js";
+import * as fruitServices from "../api/fruits.js";
+
+const detailsTemplate = (fruit, user, isOwner) => html`
+  <section id="details">
+    <div id="details-wrapper">
+      <img id="details-img" src=${fruit.imageUrl} alt="example1" />
+      <p id="details-title">${fruit.name}</p>
+      <div id="info-wrapper">
+        <div id="details-description">
+          <p>${fruit.description}</p>
+          <p id="nutrition">Nutrition</p>
+          <p id="details-nutrition">${fruit.nutrition}</p>
+        </div>
+        ${user && isOwner
+        ? html`
+              <div id="action-buttons">
+                <a href="/edit/${fruit._id}" id="edit-btn">Edit</a>
+                <a href="javascript:void(0)" id="delete-btn"
+                  >Delete</a
+                >
+              </div>
+            `
+        : nothing}
+      </div>
+    </div>
+  </section>
+`;
+
+export async function detailsPage(ctx) {
+    const dataId = ctx.params.id;
+
+    const fruit = await fruitServices.getFruit(dataId);
+
+    const isOwner = ctx.user && ctx.user._id === fruit._ownerId;
+
+    ctx.render(detailsTemplate(fruit, ctx.user, isOwner));
+}
