bf788dd180bf8fa80f93bd64f6e96ffd6585e0ba
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-04 14:13:17 +0200

    Added Register, Login and Logout functionality

--- JS Application- Exams/JS Applications Retake Exam - 8 August 2023/Samurider/src/app.js
+++ JS Application- Exams/JS Applications Retake Exam - 8 August 2023/Samurider/src/app.js
@@ -4,10 +4,21 @@ import { addRender } from './middlewares/render.js';
 import { addSession } from './middlewares/session.js';
 
 import { homePage } from './views/home.js';
+import { registerPage } from './views/register.js';
+import { loginPage } from './views/login.js';
+import { logout } from './api/users.js';
 
 page(addSession);
 page(addRender);
 
 page('/', homePage);
+page('/register', registerPage);
+page('/login', loginPage);
+page('/logout', onLogout);
 
 page.start();
+
+function onLogout(ctx) {
+    logout();
+    ctx.page.redirect('/');
+}
