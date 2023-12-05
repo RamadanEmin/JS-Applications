46c597d0eeaf2637fdb55777381e7ce987afb7d5
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-05 11:42:07 +0200

    Added Register, Login and Logout functionality

--- JS Application- Exams/JS Applications Exam - 2 Dec 2023/Character Ring_Resources/src/app.js
+++ JS Application- Exams/JS Applications Exam - 2 Dec 2023/Character Ring_Resources/src/app.js
@@ -5,12 +5,22 @@ import { addSession } from './middlewares/session.js';
 
 import { homePage } from './views/home.js';
 import { catalogPage } from './views/catalog.js';
+import { registerPage } from './views/register.js';
+import { loginPage } from './views/login.js';
+import { logout } from './api/users.js';
 
 page(addSession);
 page(addRender);
 
 page('/', homePage);
 page('/catalog', catalogPage);
-
+page('/register', registerPage);
+page('/login', loginPage);
+page('/logout', onLogout);
 
 page.start();
+
+function onLogout(ctx) {
+    logout();
+    ctx.page.redirect('/');
+}
